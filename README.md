b7a0129e649562964aab82f55d2bfcab14a968ac
Nebiyu Samuel <neba.samuel17@gmail.com>
2021-07-27 11:10:26 +0300

    modified readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # abtest_mlops
+The main objective of this project is to test if the ads that the advertising company runs resulted in a significant lift in brand awareness.
+
+# Directory Structure
+
+## notebooks
+Notebook model files are found in this directory
+## Data
+The csv data used is found in this directory
+## scripts
+Test.py can be found here
--- scripts/test.py
+++ scripts/test.py
@@ -9,9 +9,9 @@ import mlflow
 import mlflow.sklearn
 # Get url from Dvc
 import dvc.api
-path = 'data/AdSmartABdata2.csv'
+path = 'data/AdSmartABdatamplat.csv'
 repo = '/home/neba/Desktop/abtest_mlops'
-version = 'V_brow '
+version = 'version_two_platform'
 data_url = dvc.api.get_url(
     path= path,
     repo= repo,
@@ -23,3 +23,29 @@ mlflow.log_param('data_url',data_url)
 mlflow.log_param('data_version',version)
 mlflow.log_param('input_cols',data.shape[1])
 mlflow.log_param('input_rows',data.shape[0])
+# droping the users that did not answer
+def drop_no_responds(df):
+    data = df.query("not (yes == 0 & no == 0)")
+    return data
+cleaneddata = drop_no_responds(data)
+# creating new column called aware
+data['aware'] = data['yes'].map(lambda x: x==1)
+# dropping yes and no column
+data = data.drop(columns = ['yes', 'no', 'auction_id'], axis=1)
+data.head()
+# splitting data to train and test 
+y = data['aware']
+data = data.drop('aware', inplace=False, axis=1)
+X = data
+test_size = 0.1
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+y_train = pd.DataFrame(y_train)
+
+# Log artifacts: columns used for modeling
+cols_x = pd.DataFrame(list(X_train.columns))
+cols_x.to_csv('features.csv', header= False, index=False)
+mlflow.log_artifact('features.csv')
+
+cols_y = pd.DataFrame(list(y_train.columns))
+cols_y.to_csv('targets.csv', header= False, index=False)
+mlflow.log_artifact('targets.csv')
